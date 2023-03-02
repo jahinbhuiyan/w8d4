bf4b2220cd641c01a78a07ebe31b941e1201dc72
Alex L <luckyleck29@gmail.com>
2023-03-02 16:21:13 -0500

    Clock finished

--- clock.js
+++ clock.js
@@ -1,43 +1,60 @@
 class Clock {
-     constructor() {
-       const date = new Date();
-       this.hours = date.getHours();
-       this.minutes = date.getMinutes();
-       this.seconds = date.getSeconds();
-  
-       setInterval(() => {
-        //  const date = new Date();
-        //  console.log(date)
-          this.tick();
-       }, 1000);
-     }
-  
-      tick() {
-        this.seconds++;
-        if (this.seconds >= 60) {
-          this.seconds = 0;
-          this.minutes++;
-          if (this.minutes >= 60) {
-            this.minutes = 0;
-            this.hours++;
-            if (this.hours >= 24) {
-              this.hours = 0;
-            }
-          }
+  constructor() {
+    const date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+
+    // setInterval(() => {
+    //   //  const date = new Date();
+    //   //  console.log(date)
+    //   this._tick();
+    // }, 1000);
+  }
+
+  start() {
+    setInterval(() => {
+      //  const date = new Date();
+      //  console.log(date)
+      this._tick();
+    }, 1000);
+  }
+
+
+  printTime() {
+
+    let partOfDay = this.hours > 12 ? "PM" : "AM"
+
+    const time = `${this.hours % 12}:${this.minutes}:${this.seconds} ${partOfDay}`;
+    console.clear();
+    console.log(time);
+
+  }
+
+  _tick() {
+    this.seconds++;
+    if (this.seconds >= 60) {
+      this.seconds = 0;
+      this.minutes++;
+      if (this.minutes >= 60) {
+        this.minutes = 0;
+        this.hours++;
+        if (this.hours >= 24) {
+          this.hours = 0;
         }
-  
-        const time = `${this.format(this.hours)}:${this.format(this.minutes)}:${this.format(this.seconds)}`;
-        console.clear();
-        console.log(time);
-      }
-  
-      format(num) {
-        return num < 10 ? `0${num}` : num;
       }
     }
+
+    this.printTime()
+    
+  } // END 
   
-    const clock = new Clock();
-   
-  
-  
-  
+}
+
+
+
+
+
+
+
+
